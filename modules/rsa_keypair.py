dd46c0e2c97bfdd9343b8527487ab6e44bf2ad83
Bopbap <bopbap0402@gmail.com>
2024-09-12 09:30:35 +0700

    update rsa_keypair

--- modules/rsa_keypair.py
+++ modules/rsa_keypair.py
@@ -4,43 +4,63 @@ from cryptography.hazmat.backends import default_backend
 import getpass  # For securely obtaining the passphrase
 
 
-def generate_rsa_keypair():
-    # Prompt the user for a passphrase
-    passphrase = getpass.getpass("Enter a passphrase to encrypt the private key: ")
+class RSAKeyPair:
+    def __init__(self, key_size=2048, passphrase=None):
+        self.key_size = key_size
+        self.passphrase = passphrase or getpass.getpass("Enter a passphrase to encrypt the private key: ")
+        self.private_key = None
+        self.public_key = None
 
-    # Generate RSA private key
-    private_key = rsa.generate_private_key(
-        public_exponent=65537,
-        key_size=2048,  # You can increase key size if needed
-        backend=default_backend()
-    )
+    def generate_keypair(self):
+        # Generate RSA private key
+        self.private_key = rsa.generate_private_key(
+            public_exponent=65537,
+            key_size=self.key_size,
+            backend=default_backend()
+        )
+        self.public_key = self.private_key.public_key()
 
-    # Serialize private key to PKCS#8 format with encryption
-    private_key_pkcs8 = private_key.private_bytes(
-        encoding=serialization.Encoding.PEM,
-        format=serialization.PrivateFormat.PKCS8,
-        encryption_algorithm=serialization.BestAvailableEncryption(passphrase.encode())
-    )
+    def serialize_private_key(self, format='PKCS8'):
+        # Determine the private key format
+        if format == 'PKCS8':
+            private_key_format = serialization.PrivateFormat.PKCS8
+        elif format == 'TraditionalOpenSSL':
+            private_key_format = serialization.PrivateFormat.TraditionalOpenSSL
+        else:
+            raise ValueError("Invalid private key format. Choose 'PKCS8' or 'TraditionalOpenSSL'.")
 
-    # Get the public key
-    public_key = private_key.public_key()
+        # Serialize private key with encryption
+        private_key_bytes = self.private_key.private_bytes(
+            encoding=serialization.Encoding.PEM,
+            format=private_key_format,
+            encryption_algorithm=serialization.BestAvailableEncryption(self.passphrase.encode())
+        )
+        return private_key_bytes
 
-    # Serialize public key to X.509 format (PEM encoded)
-    public_key_x509 = public_key.public_bytes(
-        encoding=serialization.Encoding.PEM,
-        format=serialization.PublicFormat.SubjectPublicKeyInfo
-    )
+    def serialize_public_key(self, format='X509'):
+        # Determine the public key format
+        if format == 'X509':
+            public_key_format = serialization.PublicFormat.SubjectPublicKeyInfo
+        else:
+            raise ValueError("Invalid public key format. Choose 'X509'.")
 
-    return private_key_pkcs8, public_key_x509
+        # Serialize public key
+        public_key_bytes = self.public_key.public_bytes(
+            encoding=serialization.Encoding.PEM,
+            format=public_key_format
+        )
+        return public_key_bytes
 
 
 # Usage
-private_key, public_key = generate_rsa_keypair()
+rsa_keypair = RSAKeyPair(key_size=2048)
+rsa_keypair.generate_keypair()
+private_key = rsa_keypair.serialize_private_key(format='PKCS8')
+public_key = rsa_keypair.serialize_public_key(format='X509')
 
-# Save private key to a file
-with open("private_key_pkcs8.pem", "wb") as priv_file:
-    priv_file.write(private_key)
 
-# Save public key to a file
-with open("public_key_x509.pem", "wb") as pub_file:
-    pub_file.write(public_key)
+# Print private key to screen
+print(private_key.decode())
+
+# Print public key to screen
+print(public_key.decode())
